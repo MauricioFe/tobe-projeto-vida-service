2ec33569771bd51a91d0ae22e4181e9e27910e93
MauricioFe <mauricio.lacerdaml@gmail.com>
2021-12-09 10:40:59 -0300

    Criação da interface para conexao com o banco

---
+++ ProjetoVidaTOBE_Server/ProjetoVidaTOBE_Server/Data/IDataAccess.cs
@@ -0,0 +1,17 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoVidaTOBE_Server.Data
+{
+    interface IDataAccess
+    {
+        MySqlConnection Connection { get; }
+        DataTable GetData(string sql, params MySqlParameter[] parameters);
+        DataRow GetRow(string sql, params MySqlParameter[] parameters);
+        long GetCount(string sql, params MySqlParameter[] parameters);
+    }
+}
