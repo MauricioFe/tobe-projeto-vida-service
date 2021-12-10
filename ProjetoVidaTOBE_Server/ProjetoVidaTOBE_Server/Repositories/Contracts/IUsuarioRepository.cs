18c75038e6cd9a9ffb25ed5f7e75d263013b9545
Maurício Fonseca <mauricio.lacerdaml@gmail.com>
2021-12-10 08:53:15 -0300

    Adicionando interfaces para manipulação das consultas

---
+++ ProjetoVidaTOBE_Server/ProjetoVidaTOBE_Server/Repositories/Contracts/IUsuarioRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoVidaTOBE_Server.Repositories.Contracts
+{
+    interface IUsuarioRepository : IGenericRepository
+    {
+    }
+}
---
+++ ProjetoVidaTOBE_Server/ProjetoVidaTOBE_Server/Repositories/IGenericRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoVidaTOBE_Server.Repositories
+{
+    interface IGenericRepository<T>
+    {
+        public T Adiciona(T t);
+    }
+}
