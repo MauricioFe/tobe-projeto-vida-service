66b50dbb7cd6f0ff20bae459ad816793269115d1
MauricioFe <mauricio.lacerdaml@gmail.com>
2021-12-13 16:05:40 -0300

    Criando classes utilitárias dentro do projeto.
    Classe para gravação de logs
    Classe para as Constantes do projeto
    Classe para Mapear DataRow em um objeto
    Classe para manipular pastas

---
+++ ProjetoVidaTOBE_Server/ProjetoVidaTOBE_Server/Utils/Mapper/MapperDataRowObjectUtil.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoVidaTOBE_Server.Utils.Mapper
+{
+    public static class MapperDataRowObjectUtil
+    {
+        public static T CreateItemFromRow<T>(this DataRow row, string nomeCampo)
+        {
+            try
+            {
+                if (!row.Table.Columns.Contains(nomeCampo)) return default(T);
+
+                if ((row[nomeCampo] == null) || (row[nomeCampo] == DBNull.Value))
+                    return default(T);
+
+                return (T)Convert.ChangeType(row[nomeCampo], typeof(T), CultureInfo.CurrentCulture);
+            }
+            catch (Exception)
+            {
+                return default(T);
+            }
+        }
+    }
+}
