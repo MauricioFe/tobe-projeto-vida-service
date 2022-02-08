a45f594d9b34f61265354ccbac4b2844fad0cbd2
Maurício Fonseca <mauricio.lacerdaml@gmail.com>
2022-02-07 23:37:40 -0300

    Mapping user created

--- ProjetoVidaTOBE_Server/UserService/Models/Users.cs
+++ ProjetoVidaTOBE_Server/UserService/Models/Users.cs
@@ -2,5 +2,8 @@
 {
     public class Users
     {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
     }
 }
---
+++ ProjetoVidaTOBE_Server/UserService/Profiles/UserProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserApi.Data.Dtos;
+using UserApi.Models;
+
+namespace UserApi.Profiles
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<CreateUserDto, Users>().AfterMap((src, dest) => dest.Username = src.Email);
+            CreateMap<Users, IdentityUser<int>>();
+        }
+    }
+}
