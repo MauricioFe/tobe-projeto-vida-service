0b0bce2770060dfc13d12609205609b3543f42fe
Maurício Fonseca <mauricio.lacerdaml@gmail.com>
2022-02-11 20:26:42 -0300

    Creating a email service for activation of account

--- ProjetoVidaTOBE_Server/UserService/Services/EmailService.cs
+++ ProjetoVidaTOBE_Server/UserService/Services/EmailService.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using MailKit.Net.Smtp;
+using Microsoft.Extensions.Configuration;
+using MimeKit;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,13 +18,50 @@ namespace UserApi.Services
             _configuration = configuration;
         }
 
-        //public void SendEmail(string[] addresses, string subject,
-        //   int userId, string code, string userName)
-        //{
-        //    Message message = new Message(addresses,
-        //        subject, userName, userId, code);
-        //    var messageEmail = CreateBodyEmail(message);
-        //    Send(messageEmail);
-        //}
+        public void SendEmail(string[] addresses, string subject,
+           int userId, string code, string userName)
+        {
+            Message message = new Message(addresses,
+                subject, userName, userId, code);
+            var messageEmail = CreateBodyEmail(message);
+            Send(messageEmail);
+        }
+
+        private void Send(MimeMessage messageEmail)
+        {
+            using (var client = new SmtpClient())
+            {
+                try
+                {
+                    client.Connect(_configuration.GetValue<string>("EmailSettings:SmtpServer"), _configuration.GetValue<int>("EmailSettings:Port"), true);
+                    client.AuthenticationMechanisms.Remove("XOUATH2");
+                    client.Authenticate(_configuration.GetValue<string>("EmailSettings:From"), _configuration.GetValue<string>("EmailSettings:Password"));
+                    client.Send(messageEmail);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                    throw;
+                }
+                finally
+                {
+                    client.Disconnect(true);
+                    client.Dispose();
+                }
+            }
+        }
+
+        private MimeMessage CreateBodyEmail(Message message)
+        {
+            var emailMessage = new MimeMessage();
+            emailMessage.From.Add(new MailboxAddress("Centro Tobe", _configuration.GetValue<string>("EmailSettings:From")));
+            emailMessage.To.AddRange(message.Addresses);
+            emailMessage.Subject = message.Subject;
+            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Text)
+            {
+                Text = message.Content
+            };
+            return emailMessage;
+        }
     }
 }
