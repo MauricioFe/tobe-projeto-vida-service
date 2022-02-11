e94e89ba282a2ae9ed4b312295c291355fddab2b
Maurício Fonseca <mauricio.lacerdaml@gmail.com>
2022-02-11 20:35:43 -0300

    Creation of a logout rule in api

---
+++ ProjetoVidaTOBE_Server/UserService/Services/LogoutService.cs
@@ -0,0 +1,32 @@
+﻿using FluentResults;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserApi.Models;
+
+namespace UserApi.Services
+{
+    public class LogoutService
+    {
+        private SignInManager<IdentityUserToBe> _signInManager;
+
+        public LogoutService(SignInManager<IdentityUserToBe> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        public Result LogoutUser()
+        {
+            Task resultadoIdentity = _signInManager.SignOutAsync();
+
+            if (resultadoIdentity.IsCompletedSuccessfully)
+            {
+                return Result.Ok();
+            }
+            return Result.Fail("Logout is failure");
+
+        }
+    }
+}
