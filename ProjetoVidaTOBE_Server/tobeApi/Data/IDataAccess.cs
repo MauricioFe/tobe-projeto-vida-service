ad14bc4558a5933986da6dbf574bf5a0a97ba2af
Maurício Fonseca <mauricio.lacerdaml@gmail.com>
2022-04-25 21:15:42 -0300

    Creation of the project struture and db connection class

---
+++ ProjetoVidaTOBE_Server/tobeApi/Data/IDataAccess.cs
@@ -0,0 +1,18 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace tobeApi.Data
+{
+    public interface IDataAccess
+    {
+        MySqlConnection Connection { get; }
+        DataTable GetTable(string sql, params MySqlParameter[] parameters);
+        DataRow GetRow(string sql, params MySqlParameter[] parameters);
+        long GetCount(string sql, params MySqlParameter[] parameters);
+        void ExecuteCommand(string sql, params MySqlParameter[] parameters);
+    }
+}
